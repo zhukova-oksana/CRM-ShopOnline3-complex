b3aefb57ec1ed4731e280a2fb5e7fd56ef2a3c67
zhukova_oksana <ksy2905@gmail.com>
2022-08-07 09:00:39 +0300

    edit error tasksHard_2

--- js/tasksHard_2.js
+++ js/tasksHard_2.js
@@ -2,15 +2,24 @@
 
 const income2 = +prompt('Введите полученный заработок');
 
-if (income2 <= 15000) {
-  const tax = income2 * 13 / 100;
-  console.log(`Налог равен: ${tax}`);
-} if ((income2 > 15000) && (income2 <= 50000)) {
-    const tax20 = (income2 - 15000) * 20 / 100;
-    console.log(`Налог равен: ${tax20}`);
-} if (income2 > 50000) {
-    const tax30 = (income2 - 50000) * 30 / 100;
-    console.log(`Налог равен: ${tax30}`);
+let tax = 0;
+
+if ((income2 > 0) && (income2 <= 15000)) {
+  tax = tax + (income2 * 13 / 100);
+} else {
+  if ((income2 > 15000) && (income2 <= 50000)) {
+    tax = tax + (15000 * 13 / 100);
+    const difference = income2 - 15000;
+    tax = tax + (difference * 20 / 100);
+  } else {
+    if (income2 > 50000) {
+      tax = tax + (15000 * 13 / 100);
+      tax = tax + ((50000 - 15000) * 20 / 100);
+      const difference2 = income2 - 50000;
+      tax = tax + (difference2 * 30 / 100);
+    }
+  }
 }
 
+console.log(`Налог равен: ${tax}`);
 
