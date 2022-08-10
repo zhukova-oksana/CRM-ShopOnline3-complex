e5e7c72cdb7631984869a2cd42eafbf4b3e6f926
zhukova_oksana <ksy2905@gmail.com>
2022-08-10 08:58:02 +0300

    delete fonts

--- js/tasksHard_2.js
+++ js/tasksHard_2.js
@@ -4,20 +4,40 @@ const income2 = +prompt('Введите полученный заработок'
 
 let tax = 0;
 
-if ((income2 > 0) && (income2 <= 15000)) {
-  tax = tax + (income2 * 0.13);
-}
+// if ((income2 > 0) && (income2 <= 15000)) {
+//   tax = tax + (income2 * 0.13);
+// }
+//
+// if ((income2 > 15000) && (income2 <= 50000)) {
+//     tax = tax + (15000 * 0.13);
+//     tax = tax + ((income2 - 15000) * 0.2);
+// }
+//
+// if (income2 > 50000) {
+//     tax = tax + (15000 * 0.13);
+//     tax = tax + ((50000 - 15000) * 0.2);
+//     tax = tax + ((income2 - 50000) * 0.3);
+// }
 
-if ((income2 > 15000) && (income2 <= 50000)) {
-    tax = tax + (15000 * 0.13);
-    tax = tax + ((income2 - 15000) * 0.2);
-}
-
-if (income2 > 50000) {
-    tax = tax + (15000 * 0.13);
-    tax = tax + ((50000 - 15000) * 0.2);
-    tax = tax + ((income2 - 50000) * 0.3);
-}
+// switch (income2) {
+//
+//   case (income2 > 0): {
+//     console.log('tut');
+//     tax = tax + (income2 * 0.13);
+//     // break;
+//   }
+//
+//   case ((income2 > 15000) && (income2 <= 50000)): {
+//     tax = tax + ((income2 - 15000) * 0.2);
+//     // break;
+//   }
+//
+//   case (income2 > 50000): {
+//     tax = tax + ((income2 - 50000) * 0.3);
+//     // break;
+//   }
+//
+// }
 
 console.log(`Налог равен: ${tax}`);
 
